3b94e1de2a4e9469997c84ffda27f76ec5ebb8e0
delongZhai <zhai226106433@gmail.com>
2019-04-19 20:36:15 -0400

    testing the feature after merges, and insert a user into database

--- README.md
+++ README.md
@@ -4,7 +4,5 @@ A facebook replica with limited functionalities created by Delong Zhai and Bhavi
 The project, "facebook-replica", is a final project in CIS 4160.
 It includes limited but important features in Facebook, such as,
  
---- User can sign in, sign up through index.php
---- User are directed to user's home page once signed in
---- 
---- 
+1. User can sign in, sign up through index.php
+2. User are directed to user's home page once signed in
--- public/login.php
+++ public/login.php
@@ -5,7 +5,7 @@
     <title>Facebook -Log In or Sign Up</title>
 		<h1 id="white" class="facebook">facebook</h1>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link href="styles.css" rel="stylesheet" type="text/css"/>
+		<link href="styles/styles.css" rel="stylesheet" type="text/css"/>
 </head>
 
 <body>
